ba9265a687dc27802785866fd1f099478e33d0c2
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-06-19 17:38:03 -0400

    Add parsing of docstring.

--- tutorial/utils/convert_props_to_list.py
+++ tutorial/utils/convert_props_to_list.py
@@ -13,22 +13,45 @@ from textwrap import dedent
 def generate_prop_info(component_name, lib=dcc):
     component = getattr(lib, component_name)
     component_doc = component.__doc__
-    subs = [
-        [r'A {} component.\n'.format(component.__name__), ''],
-        [r'- setProps.*\n', ''],
-        [r'Available events: .*', '']
-    ]
-    for sub in subs:
-        component_doc = re.sub(sub[0], sub[1], component_doc)
 
-    return html.Div([
+    regex = r"^\s*([a-zA-Z_]*)\s*\(([a-zA-Z\/:',\s]*);\s*([A-Za-z\s]*)\):\s*(.*?)\s*$"
+
+    return_div = [
         dcc.Markdown(dedent(
-        '''
-        > Access this documentation in your Python terminal with:
-        > ```
-        > >>> help({}.{})
-        > ```
-        '''.format(lib.__name__, component_name)
-        )),
-        dcc.Markdown(component_doc)
-    ])
+            '''
+            > Access this documentation in your Python terminal with:
+            > ```
+            > >>> help({}.{})
+            > ```
+            '''.format(lib.__name__, component_name)
+        ))
+    ]
+
+    props = component_doc.split('\n-')[1:]
+
+    for prop in props:
+        if component_name == 'Molecule3dViewer' and 'dash_bio' in str(lib):
+            print(component_doc)
+
+        r = re.match(
+            re.compile(regex),
+            prop.replace('\n', ' ')
+        )
+
+        if r is not None:
+            (prop_name, prop_type, prop_optional_default, prop_desc) = r.groups()
+            prop_desc = prop_desc.replace('[', '\[').replace(']', '\]')
+
+            defined_default_val = re.search(r'default (.*)', prop_optional_default)
+
+            prop_optional = prop_optional_default
+            if defined_default_val is not None:
+                prop_optional = f'default `{defined_default_val.groups(1)[0]}`'
+
+            return_div.append(dcc.Markdown(dedent(
+                f'''
+                **`{prop_name}`** (*{prop_type};* {prop_optional}): {prop_desc}
+                '''
+            )))
+
+    return html.Div(return_div)
