5529a6e14ddd6615c88c6eaea3ab8692760f35d9
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-06-20 14:46:26 -0400

    Add regex for parsing nested props.

--- tutorial/utils/convert_props_to_list.py
+++ tutorial/utils/convert_props_to_list.py
@@ -9,7 +9,7 @@ def generate_prop_info(component_name, lib=dcc):
     component = getattr(lib, component_name)
     component_doc = component.__doc__
 
-    regex = r"^\s*([a-zA-Z_]*)\s*\(([a-zA-Z\/:',\s]*);\s*([A-Za-z\s]*)\):\s*(.*?)\s*$"
+    regex = r'''^\s*([a-zA-Z_]*)\s*\(([a-zA-Z\/:',.()\s]*);*\s*([\w\s'"().]*)\):\s*(.*?)\s*$'''
 
     return_div = [
         dcc.Markdown(dedent(
@@ -24,9 +24,12 @@ def generate_prop_info(component_name, lib=dcc):
 
     props = component_doc.split('\n-')[1:]
 
+    # sort alphabetically, but keep id at the top
+    id_prop = props.pop(0)
+    props.sort(key=lambda x: x.strip()[0])
+    props = [id_prop] + props
+
     for prop in props:
-        if component_name == 'Molecule3dViewer' and 'dash_bio' in str(lib):
-            print(component_doc)
 
         r = re.match(
             re.compile(regex),
@@ -37,16 +40,35 @@ def generate_prop_info(component_name, lib=dcc):
             (prop_name, prop_type, prop_optional_default, prop_desc) = r.groups()
             prop_desc = prop_desc.replace('[', '\[').replace(']', '\]')
 
-            defined_default_val = re.search(r'default (.*)', prop_optional_default)
+            if 'dict containing keys' in prop_desc:
+                regex_dict = r'''.*\. [\w]* has the following type: (?:[\w\s|]*)dict containing keys ([\w\s',]*). Those keys have the following types: ([\w\s|();:',.-]*)'''
+                try:
+                    top_level_keys = re.match(
+                        re.compile(regex_dict),
+                        prop_desc
+                    ).groups(1)[0]
+                    key_defs = re.match(re.compile(regex_dict), prop_desc).groups(1)[1]
+                    top_level_keys = [key.strip().strip('\'')
+                                      for key in top_level_keys.split(',')]
+                    for key in top_level_keys:
+                        key_defs = key_defs.replace(f'- {key}', f'\n- {key}', 1)
+                        prop_desc_list = ''.join(key_defs)
+                    prop_desc = prop_desc_list
+                except AttributeError:
+                    pass
+
+            defined_default_val = re.search(
+                r'''default ([\w\s'.()"]*)''',
+                prop_optional_default
+            )
 
             prop_optional = prop_optional_default
             if defined_default_val is not None:
-                prop_optional = f'default `{defined_default_val.groups(1)[0]}`'
+                default_val = defined_default_val.groups(1)[0]
+                prop_optional = f'default `{default_val}`'
 
             return_div.append(dcc.Markdown(dedent(
-                f'''
-                **`{prop_name}`** (*{prop_type};* {prop_optional}): {prop_desc}
-                '''
+                f'''**`{prop_name}`** (*{prop_type};* {prop_optional}): {prop_desc}'''
             )))
 
     return html.Div(return_div)
