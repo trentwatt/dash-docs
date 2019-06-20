682a969e411feb9dcb18bff92615f591dfa42a09
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-06-20 14:03:14 -0400

    Remove unnecessary imports.

--- tutorial/utils/convert_props_to_list.py
+++ tutorial/utils/convert_props_to_list.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-import json
-import os
 import dash_html_components as html
 import dash_core_components as dcc
-import dash_cytoscape
-import dash_table
-import pandas as pd
 import re
 from textwrap import dedent
 
