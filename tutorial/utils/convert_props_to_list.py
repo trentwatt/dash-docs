7bfa86a58de75cbc6d126821ff00ae9dded90848
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-06-21 17:05:26 -0400

    Add docstrings even if they don't match with regex.
    Move code for case in which there is a regex match out of an 'if' clause and dedent it.

--- tutorial/utils/convert_props_to_list.py
+++ tutorial/utils/convert_props_to_list.py
@@ -36,39 +36,49 @@ def generate_prop_info(component_name, lib=dcc):
             prop.replace('\n', ' ')
         )
 
-        if r is not None:
-            (prop_name, prop_type, prop_optional_default, prop_desc) = r.groups()
-            prop_desc = prop_desc.replace('[', '\[').replace(']', '\]')
+        if r is None:
+            return_div.append(dcc.Markdown(dedent(prop.replace('\n', ' '))))
+            continue
 
-            if 'dict containing keys' in prop_desc:
-                regex_dict = r'''.*\. [\w]* has the following type: (?:[\w\s|]*)dict containing keys ([\w\s',]*). Those keys have the following types: ([\w\s|();:',.-]*)'''
-                try:
-                    top_level_keys = re.match(
-                        re.compile(regex_dict),
-                        prop_desc
-                    ).groups(1)[0]
-                    key_defs = re.match(re.compile(regex_dict), prop_desc).groups(1)[1]
-                    top_level_keys = [key.strip().strip('\'')
-                                      for key in top_level_keys.split(',')]
-                    for key in top_level_keys:
-                        key_defs = key_defs.replace(f'- {key}', f'\n- {key}', 1)
-                        prop_desc_list = ''.join(key_defs)
-                    prop_desc = prop_desc_list
-                except AttributeError:
-                    pass
-
-            defined_default_val = re.search(
-                r'''default ([\w\s'.()"]*)''',
-                prop_optional_default
+        (prop_name, prop_type, prop_optional_default, prop_desc) = r.groups()
+        prop_desc = prop_desc.replace('[', '\[').replace(']', '\]')
+        if 'dict containing keys' in prop_desc:
+            regex_dict = r'''(.*?\. [\w]* has the following type: (?:[\w\s|]*)dict containing keys )([\w\s',]*)(\. Those keys have the following types: )([\w\s|();:',.-]*)'''
+            parsed_dict_desc = re.match(
+                re.compile(regex_dict),
+                prop_desc
             )
+            try:
+                top_level_desc = parsed_dict_desc.groups(0)[0]
+                top_level_keys = parsed_dict_desc.groups(0)[1]
+                top_level_type_preamble = parsed_dict_desc.groups(0)[2]
+                key_defs = parsed_dict_desc.groups(0)[3]
 
-            prop_optional = prop_optional_default
-            if defined_default_val is not None:
-                default_val = defined_default_val.groups(1)[0]
-                prop_optional = f'default `{default_val}`'
+                top_level_keys_list = [key.strip().strip('\'')
+                                       for key in top_level_keys.split(',')]
+                for key in top_level_keys_list:
+                    key_defs = key_defs.replace(f'- {key}', f'\n- {key}', 1)
 
-            return_div.append(dcc.Markdown(dedent(
-                f'''**`{prop_name}`** (*{prop_type};* {prop_optional}): {prop_desc}'''
-            )))
+                prop_desc = top_level_desc + \
+                    top_level_keys + \
+                    top_level_type_preamble + \
+                    ''.join(key_defs)
+
+            except AttributeError:
+                pass
+
+        defined_default_val = re.search(
+            r'''default (.*)''',
+            prop_optional_default
+        )
+
+        prop_optional = prop_optional_default
+        if defined_default_val is not None:
+            default_val = defined_default_val.groups(1)[0]
+            prop_optional = f'default `{default_val}`'
+
+        return_div.append(dcc.Markdown(dedent(
+            f'''**`{prop_name}`** (*{prop_type};* {prop_optional}): {prop_desc}'''
+        )))
 
     return html.Div(return_div)
